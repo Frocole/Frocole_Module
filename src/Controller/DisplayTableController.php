e1b59166b1b72be4a533bfdc2ef896070567ea1b
wvdvegt <wim@vander-vegt.nl>
2021-07-01 15:14:22 +0200

    1) Initial Commit.

---
+++ src/Controller/DisplayTableController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace Drupal\mymodule\Controller;
+
+use Drupal\Core\Database\Database;
+use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Link;
+use Drupal\Core\Url;
+
+/**
+ * Class DisplayTableController
+ *
+ * @package Drupal\mymodule\Controller
+ */
+class DisplayTableController extends ControllerBase
+{
+
+    public function index()
+    {
+        //create table header
+        $header_table = array(
+        'CourseID' => t('Course ID'),
+        'CourseName' => t('Course Name'),
+        'IPF_RD_parameters' => t('Individual Performance'),
+        'GPF_RD_parameters' => t('Group Performance'),
+        'LeraarUserID' => t('leraar'),
+        'CourseActive' => t('active'),
+
+        'view' => t('View'),
+        'delete' => t('Delete'),
+        'edit' => t('Edit'),
+        );
+
+
+        // get data from database
+        $query = Database::getConnection('default', 'frocole')->select('courses', 'c');
+        $query->fields('c', ['CourseID', 'CourseName', 'IPF_RD_parameters', 'GPF_RD_parameters', 'LeraarUserID', 'CourseActive']);
+        $results = $query->execute()->fetchAll();
+
+        $rows = array();
+        foreach ($results as $data) {
+            $url_delete = Url::fromRoute('mymodule.delete_form', ['id' => $data->CourseID], []);
+            $url_edit = Url::fromRoute('mymodule.add_form', ['id' => $data->CourseID], []);
+            $url_view = Url::fromRoute('mymodule.show_data', ['id' => $data->CourseID], []);
+
+            $linkDelete = Link::fromTextAndUrl('Delete', $url_delete);
+            $linkEdit = Link::fromTextAndUrl('Edit', $url_edit);
+            $linkView = Link::fromTextAndUrl('View', $url_view);
+
+            //get data
+            $rows[] = array(
+            'CourseID' => $data->CourseID,
+            'CourseName' => $data->CourseName,
+            'IPF_RD_parameters' => $data->IPF_RD_parameters,
+            'GPF_RD_parameters' => $data->GPF_RD_parameters,
+            'LeraarUserID' => $data->LeraarUserID,
+            'CourseActive' => $data->CourseActive,
+
+            'view' => $linkView,
+            'delete' => $linkDelete,
+            'edit' =>  $linkEdit,
+            );
+
+        }
+
+        $url = Url::fromRoute('mymodule.add_form');
+
+        $form['add'] = [
+          '#type' => 'item',
+          '#markup' => '<a href="'.$url->toString().'">Add Course</a>',
+        ];
+        
+        // render table
+        $form['table'] = [
+        '#type' => 'table',
+        '#header' => $header_table,
+        '#rows' => $rows,
+        '#empty' => t('No data found'),
+        ];
+
+        return $form;
+
+    }
+
+}
---
+++ src/Form/AddForm.php
@@ -0,0 +1,160 @@
+<?php
+
+namespace Drupal\mymodule\Form;
+
+use Drupal\Core\Database\Database;
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Url;
+use Drupal\file\Entity\File;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+
+
+class AddForm extends FormBase
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getFormId()
+    {
+        return 'mymodule_form';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(array $form, FormStateInterface $form_state)
+    {
+      $url = Url::fromRoute('mymodule.display_data');
+
+      $form['index'] = [
+        '#type' => 'item',
+        '#markup' => '<a href="'.$url->toString().'">View Courses</a>',
+      ];
+
+      $conn = Database::getConnection('default', 'frocole');
+        $data = array();
+        if (isset($_GET['id'])) {
+            $query = $conn->select('courses', 'm')
+                ->condition('CourseID', $_GET['id'])
+                ->fields('m');
+            $data = $query->execute()->fetchAssoc();
+        }
+
+        // See https://api.drupal.org/api/drupal/elements/8.2.x
+
+        $form['CourseName'] = [
+        '#type' => 'textfield',
+        '#title' => $this->t('Course Name'),
+        '#required' => true,
+        '#size' => 60,
+        '#default_value' => (isset($data['CourseName'])) ? $data['CourseName'] : '',
+        '#maxlength' => 128,
+        '#wrapper_attributes' => ['class' => 'col-md-6 col-xs-12']
+        ];
+        $form['IPF_RD_parameters'] = [
+        '#type' => 'textfield',
+        '#title' => $this->t('Individual Performance'),
+        '#required' => true,
+        '#size' => 60,
+        '#default_value' => (isset($data['IPF_RD_parameters'])) ? $data['IPF_RD_parameters'] : '',
+        '#wrapper_attributes' => ['class' => 'col-md-6 col-xs-12']
+        ];
+        $form['GPF_RD_parameters'] = [
+        '#type' => 'textfield',
+        '#title' => $this->t('Group Performance'),
+        '#required' => true,
+        '#default_value' => (isset($data['GPF_RD_parameters'])) ? $data['GPF_RD_parameters'] : '',
+        '#wrapper_attributes' => ['class' => 'col-md-6 col-xs-12']
+        ];
+
+        // Find all users and their id's.
+        $form['LeraarUserID'] = [
+        '#type' => 'select',
+        '#title' => $this
+        ->t('Select leraar'),
+        '#options' => $this->FetchUsers(),
+        '#wrapper_attributes' => ['class' => 'col-md-6 col-xs-12'],
+        '#default_value' => (isset($data['LeraarUserID'])) ? $data['LeraarUserID'] : '',
+        ];
+
+        $form['CourseActive'] = [
+        '#type' => 'checkbox',
+        '#title' => $this->t('active'),
+        '#required' => true,
+        '#default_value' => (isset($data['CourseActive'])) ? $data['CourseActive'] : '',
+        '#wrapper_attributes' => ['class' => 'col-md-6 col-xs-12']
+        ];
+
+        $form['submit'] = [
+        '#type' => 'submit',
+        '#value' => $this->t('save'),
+        '#buttom_type' => 'primary'
+        ];
+
+        return $form;
+    }
+
+    /**
+     * @param array              $form
+     * @param FormStateInterface $form_state
+     */
+    public function validateForm(array &$form, FormStateInterface $form_state)
+    {
+        if (is_numeric($form_state->getValue('first_name'))) {
+            $form_state->setErrorByName('first_name', $this->t('Error, The First Name Must Be A String'));
+        }
+    }
+
+
+    /**
+     * {@inheritdoc}
+     */
+    public function submitForm(array &$form, FormStateInterface $form_state)
+    {
+        $data = array(
+        'CourseName' => $form_state->getValue('CourseName'),
+        'IPF_RD_parameters' => $form_state->getValue('IPF_RD_parameters'),
+        'GPF_RD_parameters' => $form_state->getValue('GPF_RD_parameters'),
+        'LeraarUserID' => $form_state->getValue('LeraarUserID'),
+        'CourseActive' => $form_state->getValue('CourseActive'),
+        );
+
+        if (isset($_GET['id'])) {
+            // update data in database
+            Database::getConnection('default', 'frocole')->update('courses')->fields($data)->condition('CourseID', $_GET['id'])->execute();
+        } else {
+            // insert data to database
+            Database::getConnection('default', 'frocole')->insert('courses')->fields($data)->execute();
+        }
+
+        // show message and redirect to list page
+        \Drupal::messenger()->addStatus('Succesfully saved');
+        $url = new Url('mymodule.display_data');
+        $response = new RedirectResponse($url->toString());
+        $response->send();
+    }
+
+    /**
+     * @return an associated array of user'is, their names and nicknames.
+     */
+    private function FetchUsers()
+    {
+        // [Users]
+        $query = Database::getConnection('default', 'frocole')
+            ->select('users', 'u')
+            ->fields('u', ['UserID', 'Username', 'Nickname']);
+
+        $data = $query
+            ->execute()
+            ->fetchAllAssoc('UserID', \PDO::FETCH_ASSOC);
+
+        $result = array();
+        foreach ($data as $record) {
+            // Do something with each $record
+            $result[$record['UserID']] = "[".str_pad($record['UserID'], 4, '0', STR_PAD_LEFT)."] ".$record['Username']." (".$record['Nickname'].")";
+        }
+
+        return $result;
+    }
+}
